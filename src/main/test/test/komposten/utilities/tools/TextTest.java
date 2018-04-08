e887f2d836af500d4c7b094ff929ff2c79f346e3
Komposten <j.hjelm586@hotmail.com>
2018-04-08 13:53:50 +0200

    Added a test for Text.getEditDistanceMatrix()

--- src/main/test/test/komposten/utilities/tools/TextTest.java
+++ src/main/test/test/komposten/utilities/tools/TextTest.java
@@ -1,5 +1,6 @@
 package test.komposten.utilities.tools;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
@@ -65,6 +66,26 @@ public class TextTest
 	{
 		Text.editDistance("cat", "dog", false);
 		Text.getEditDistanceOperations();
+	}
+	
+	
+	@Test
+	public void testGetEditDistanceMatrix()
+	{
+		Text.editDistance("interest", "ntvaestes", true);
+		int[][] expected = new int[][] {
+			new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8},
+			new int[] {1, 1, 1, 2, 3, 4, 5, 6, 7},
+			new int[] {2, 2, 2, 1, 2, 3, 4, 5, 6},
+			new int[] {3, 3, 3, 2, 2, 3, 4, 5, 6},
+			new int[] {4, 4, 4, 3, 3, 3, 4, 5, 6},
+			new int[] {5, 5, 5, 4, 3, 4, 3, 4, 5},
+			new int[] {6, 6, 6, 5, 4, 4, 4, 3, 4},
+			new int[] {7, 7, 7, 6, 5, 5, 5, 4, 3},
+			new int[] {8, 8, 8, 7, 6, 6, 5, 5, 4},
+			new int[] {9, 9, 9, 8, 7, 7, 6, 5, 5}};
+			
+		assertArrayEquals(expected, Text.getEditDistanceMatrix());
 	}
 	
 	
